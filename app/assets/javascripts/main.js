18cfa65b18ead166ea379b7a9ba52fb23368b281
Bipin Manandhar <kubipin07@gmail.com>
2017-10-07 20:42:56 +0545

    Add ajax code for retriving the base_url and posture size from TMDB api

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,39 @@
+$(function(){
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/search/movie?api_key=269682043d85ac69d89cdca6988cb2b9',
+    data: {"query" : "superman"}
+  })
+  .done(function(data){
+    displayMovies(data);
+  });
+
+  function displayMovies(data){
+    let htmlString = "";
+    let imageUrl = getBaseImageUrl();
+
+    data['results'].forEach(function(movie){
+      htmlString +=   `<img src= ${imageUrl}/${movie["poster_path"]}>
+                      <p>${movie["title"]}</p>
+                      <p>${movie["overview"]}</p>`;
+    });
+
+    $('#movies').append(htmlString);
+  }
+
+  function getBaseImageUrl(){
+    var url = "";
+    var settings = {
+      "async": false,
+      "crossDomain": true,
+      "url": "https://api.themoviedb.org/3/configuration?api_key=269682043d85ac69d89cdca6988cb2b9",
+      "method": "GET",
+      "headers": {},
+      "data": "{}"
+    }
+
+    $.ajax(settings).done(function(response){
+      url = response["images"]["base_url"] + response["images"]["poster_sizes"][3];
+    });
+    return url;
+  }
+})
